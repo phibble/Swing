d56fe0ee9409e3a3cb4c4aada90ca38e981cc281
phibble <phillip.hausmann@freenet.de>
2020-04-19 15:57:36 +0200

    Swing 4 - JPanel TextPanel

--- Swing/src/swing/MainFrame.java
+++ Swing/src/swing/MainFrame.java
@@ -6,13 +6,12 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame
 {
 	private static final long serialVersionUID = 1L;
 
-	private JTextArea textArea;
+	private TextPanel textPanel;
 	private JButton btn;
 
 	public MainFrame()
@@ -21,18 +20,18 @@ public class MainFrame extends JFrame
 
 		setLayout(new BorderLayout());
 
-		textArea = new JTextArea();
+		textPanel = new TextPanel();
 		btn = new JButton("Click Me!");
 
 		btn.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent e)
 			{
-				textArea.append("Hello\n");
+				textPanel.appendText("Hello\n");
 			}
 		});
 
-		add(textArea, BorderLayout.CENTER);
+		add(textPanel, BorderLayout.CENTER);
 		add(btn, BorderLayout.SOUTH);
 
 		setSize(600, 500);
